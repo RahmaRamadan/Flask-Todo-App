0af59f41482ab04e8e97c3f48b08498227d2932a
RahmaRamadan <rahmaramadan23@gmailcom>
2022-03-04 23:11:10 +0200

    add application styling

--- app.py
+++ app.py
@@ -1,38 +1,96 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
+from flask_login import UserMixin, login_required, LoginManager, current_user, logout_user
+import os
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-# ------------------------------------------------------------
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+# ---------------------------Models--------------------------
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False)
+
 # -------------------------home-----------------------------------
+
+
 @app.route('/')
 def index():
-    #show all todos
+    # show all todos
     todo_list = Todo.query.all()
     print(todo_list)
-    return render_template('base.html', todo_list = todo_list)
+    return render_template('base.html', todo_list=todo_list)
 
 # ---------------------------login------------------------------------
-@app.route('/login')
+
+
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    # post
+    user = User.query.filter_by(username=request.form.get('username')).first()
+    if not user or user.password != request.form.get('password'):
+        return render_template('login.html', errors=['username or password not found'])
+
+    else:
+        return redirect('/')
 
 # -------------------------register-----------------------------------
-@app.route('/register')
-def register():
-    return render_template('register.html')
 
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    # post
+    all = User.query.all()
+    user1 = User.query.filter_by(username=request.form.get('username')).first()
+    if user1:
+        return render_template('register.html', errors=['user already exists'])
+    else:
+        user = User(
+            username=request.form.get('username'),
+            password=request.form.get("password")
+        )
+        db.session.add(user)
+        db.session.commit()
+        return render_template('login.html')
+
+
+# -------------------------logout-----------------------------------
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 # -------------------------add-----------------------------------
+
+
 @app.route('/add', methods=["POST"])
 def add():
     title = request.form.get("title")
@@ -42,6 +100,8 @@ def add():
     return redirect(url_for("index"))
 
 # -------------------------update---------------------------------
+
+
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
@@ -50,12 +110,15 @@ def update(todo_id):
     return redirect(url_for("index"))
 
 # -------------------------delete---------------------------------
+
+
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("index"))
+
 
 # ------------------------------------------------------------
 if __name__ == "__main__":
